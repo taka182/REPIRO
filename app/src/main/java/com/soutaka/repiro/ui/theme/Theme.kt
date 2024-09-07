18a3864e3b2cf141e3b729a66967a3f5dfad345f
[taka182] <[soutakaoka92@gmail.com]>
2024-09-07 14:09:58 +0900

    update:Appのcolorを追加

--- app/src/main/java/com/soutaka/repiro/ui/theme/Theme.kt
+++ app/src/main/java/com/soutaka/repiro/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.soutaka.repiro.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,7 +8,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -48,6 +50,11 @@ fun REPIROTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setStatusBarColor( color = APP_COLOR)
     }
 
     MaterialTheme(
