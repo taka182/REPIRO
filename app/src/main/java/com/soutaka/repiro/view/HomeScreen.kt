ff05c90d8e2c9d6e0920875e8e8b6a863b36828b
[taka182] <[soutakaoka92@gmail.com]>
2024-09-07 14:07:48 +0900

    add:screen作成
    レイアウトは別で対応

--- app/src/main/java/com/soutaka/repiro/view/HomeScreen.kt
+++ app/src/main/java/com/soutaka/repiro/view/HomeScreen.kt
@@ -1,5 +1,35 @@
 package com.soutaka.repiro.view
 
-fun HomeScreen() {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.testTag
 
+
+@Composable
+fun HomeScreen() {
+    var isDarkTheme by remember { mutableStateOf(false) }
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .testTag("HomeScreen"),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            // テーマを切り替えるボタン
+            Button(onClick = {
+                isDarkTheme = !isDarkTheme
+            }) {
+                Text(text = "Toggle Theme")
+            }
+            Text(text = "Welcome to Home Screen!")
+        }
+    }
 }
